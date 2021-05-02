b4a9967b72810054fd23ab59d07ef42b6eab7b9d
Balaji K <balajikyadav.official@gmail.com>
2021-05-02 14:25:09 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter 1 for encryption or 2 for decryption : ");
         String num = scanner.nextLine();
-        String key = generateRandomKey();
+        String key;
         if (num.equals("1")) {
             System.out.print("Enter the name of the file to be encrypted : ");
             String file = scanner.nextLine();
@@ -23,9 +23,15 @@ public class Main {
             }
             int data = reader.read();
             StringBuilder str = new StringBuilder();
+            key = generateRandomKey();
+            for(int i = 0; i < 15; ++i) {
+                str.append((int)(key.charAt(i)) * 17).append(" ");
+            }
+            int j = 0;
             while (data != -1) {
-                str.append(data).append(" ");
+                str.append(data * (int)(key.charAt(j))).append(" ");
                 data = reader.read();
+                j = ++j % 15;
             }
             String newFile = "EncryptCLI." + file.substring(file.indexOf('.') + 1);
             reader.close();
@@ -48,10 +54,28 @@ public class Main {
             char data = (char)temp;
             StringBuilder str = new StringBuilder();
             StringBuilder te = new StringBuilder();
+            StringBuilder keyBuilder = new StringBuilder();
+            int j = 15;
+            while(j > 0) {
+                if (data == ' ') {
+                    keyBuilder.append((char)(Integer.parseInt(te.toString())/17));
+                    te = new StringBuilder();
+                    j--;
+                } else {
+                    te.append(data);
+                }
+                temp = reader.read();
+                data = (char)temp;
+            }
+            key = keyBuilder.toString();
+            j = 0;
             while(temp != -1) {
                 if (data == ' ') {
-                    str.append((char) Integer.parseInt(te.toString()));
+                    int data_char = Integer.parseInt(te.toString());
+                    data_char /= key.charAt(j);
+                    str.append((char)data_char);
                     te = new StringBuilder();
+                    j = ++j % 15;
                 } else {
                     te.append(data);
                 }
@@ -71,7 +95,7 @@ public class Main {
     public static String generateRandomKey() {
         StringBuilder key = new StringBuilder();
         for (int i = 0; i < 15; ++i) {
-            key.append(Math.round(Math.random() * 93 + 33));
+            key.append((char)Math.round(Math.random() * 93 + 33));
         }
         return key.toString();
     }
