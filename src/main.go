68a35ba854733d1eac74cebc4f9a692dbd07a2b2
Balaji K <balajikyadav.official@gmail.com>
2021-07-28 16:04:17 +0530

    More secure encryptions

--- src/main.go
+++ src/main.go
@@ -16,6 +16,22 @@ func generateRandomKey() string {
 	return key
 }
 
+func encode_zeroes(str string) string {
+	encoded_str := str
+	for i := 0; i < 5-len(str); i++ {
+		encoded_str = "0" + encoded_str
+	}
+	return encoded_str
+}
+
+func reverse(str string) string {
+	reversed := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		reversed += string(str[i])
+	}
+	return reversed
+}
+
 func main() {
 	fmt.Print("Enter 1 for encryption or 2 for decryption : ")
 	var num int
@@ -36,19 +52,20 @@ func main() {
 			key := generateRandomKey()
 			for i := 0; i < 15; i++ {
 				ascii_value := int(key[i])
-				encrypted += fmt.Sprintf("%d ", ascii_value*17)
+				encrypted += reverse(encode_zeroes(fmt.Sprintf("%d", ascii_value*17)))
 			}
 
 			//  Encrypt Main Document
 			for i := 0; i < len(file); i++ {
 				ascii_value := int(key[i%15])
-				encrypted += fmt.Sprintf("%d ", ascii_value*int(file[i]))
+				encrypted += reverse(encode_zeroes(fmt.Sprintf("%d", ascii_value*int(file[i]))))
 			}
+
 			os.WriteFile(fileName, []byte(encrypted), 0644)
 			fmt.Println("File has been encrypted")
 		}
 	} else if num == 2 {
-		fmt.Println("Enter the path of the file to decrypted -")
+		fmt.Println("Enter the path of the file to be decrypted -")
 		var fileName string
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
@@ -62,28 +79,27 @@ func main() {
 			var key, temp = "", ""
 			var j, i = 0, 0
 			for i = 0; j < 15; i++ {
-				if string(file[i]) == " " {
-					n, _ := strconv.Atoi(temp)
+				temp += string(file[i])
+				if (i+1)%5 == 0 {
+					n, _ := strconv.Atoi(reverse(temp))
 					key += string(rune((n / 17)))
 					temp = ""
 					j++
-				} else {
-					temp += string(file[i])
 				}
 			}
 
 			// Decrypt Main Document
 			text := ""
 			j = 0
+			k := 0
 			for ; i < len(file); i++ {
-				if string(file[i]) == " " {
-					n, _ := strconv.Atoi(temp)
+				k++
+				temp += string(file[i])
+				if k%5 == 0 {
+					n, _ := strconv.Atoi(reverse(temp))
 					text += string(rune(n / int(key[j])))
-					j++
-					j = j % 15
+					j = (j + 1) % 15
 					temp = ""
-				} else {
-					temp += string(file[i])
 				}
 			}
 
